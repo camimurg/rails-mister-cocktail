474759a660e89e1cf341a4ccae2c33d8d4158fc1
Camilla Murgia <c.murgia4@studenti.uniss.it>
2020-11-12 13:18:34 +0100

    Added all validations

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,4 +1,7 @@
 class Cocktail < ApplicationRecord
-  has_many :ingredients
-  has_many :doses
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+
+  validates :name, presence: true, uniqueness: true
+
 end
